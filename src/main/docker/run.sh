26817076b1c222050c8cec235f12327302498e52
Guiovanni Leon <Giovanni.Leon@endava.com>
2019-09-04 10:59:05 -0500

    fixing Docker File

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-getPort() {
-    echo $1 | cut -d : -f 3 | xargs basename
-}
-
-
 
 echo "********************************************************"
 echo "Starting Weather Central API"
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom  \
-     -jar /usr/local/weather-api.jar
+     -jar /usr/local/central-weather-api.jar
